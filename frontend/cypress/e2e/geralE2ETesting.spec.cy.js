16160b896f0a37329296739780230dd90564ed8a
leonardo <leomelo.py@gmail.com>
2025-07-02 15:13:57 -0300

    Adicionando o fakerjs

--- frontend/cypress/e2e/geralE2ETesting.spec.cy.js
+++ frontend/cypress/e2e/geralE2ETesting.spec.cy.js
@@ -1,29 +1,89 @@
-describe('Currículo Fácil', () => {
-  it('Entrando no local da aplicação', () => {
-    cy.visit('http://localhost:5173/')
+import { faker } from '@faker-js/faker/locale/pt_BR'; 
 
-    cy.get('h1').should('contain', 'Currículo Inteligente')
-    cy.get('#root > div > form > input[type=text]:nth-child(1)').should('exist')
-    cy.get('#root > div > form > input[type=text]:nth-child(1)').type('Leonardo Cunha')
-    cy.get('#root > div > form > input[type=tel]:nth-child(2)').should('exist')
-    cy.get('#root > div > form > input[type=tel]:nth-child(2)').type('83988447654')
-    cy.get('#root > div > form > input:nth-child(3)').should('exist')
-    cy.get('#root > div > form > input:nth-child(3)').type('flamenguista.peso@gmail.com')
-    cy.get('#root > div > form > textarea:nth-child(4)').should('exist')
-    cy.get('#root > div > form > textarea:nth-child(4)').type('https://www.youtube.com/watch?v=hQ01th1G1AE')
-    cy.get('#root > div > form > input[type=text]:nth-child(5)').should('exist')
-    cy.get('#root > div > form > input[type=text]:nth-child(5)').type('Desenvolvedor Frontend')
-    cy.get('#root > div > form > textarea:nth-child(6)').should('exist')
-    cy.get('#root > div > form > textarea:nth-child(6)').type('5 anos de experiência como tech leader na magazine luiza, 2 anos de professor de qualidade de software na unifacisa')
-    cy.get("#root > div > form > textarea:nth-child(7)").should('exist')
-    cy.get("#root > div > form > textarea:nth-child(7)").type('Cypress, React, Node.js, Java, Python, SQL, MongoDB, Docker, Kubernetes, Selenium, Git, Agile, Scrum, Kanban, Test Automation, Continuous Integration, Continuous Deployment, DevOps, Cloud Computing, Microservices, RESTful APIs, GraphQL, Web Development, Mobile Development, UI/UX Design, Performance Optimization, Security Best Practices')
-    cy.get("#root > div > form > textarea:nth-child(8)").should('exist')
-    cy.get("#root > div > form > textarea:nth-child(8)").type('Graduação completa em Ciência da Computação pela Universidade Federal de Campina Grande, MBA em Engenharia de Software pela PUC-Rio')
-    cy.get("#root > div > form > textarea:nth-child(9)").should('exist')
-    cy.get("#root > div > form > textarea:nth-child(9)").type('CTFL, AWS Practitioner, Scrum Master, Docker Certified Associate')
-    cy.get("#root > div > form > textarea:nth-child(10)").should('exist')
-    cy.get("#root > div > form > textarea:nth-child(10)").type('Ingles B2, Portugues Nativo, Espanhol A2')
-    cy.get("#root > div > form > button").should('exist')
-    cy.get("#root > div > form > button").click()
-  })
-})
+describe('Currículo Fácil - Geração de Currículo Inteligente com Faker.js', () => {
+  const APP_URL = 'http://localhost:5173/';
+
+  const selectors = {
+    nameInput: 'input[type=text]:nth-child(1)',
+    phoneInput: '#root > div > form > input[type=tel]:nth-child(2)',
+    emailInput: 'input:nth-child(3)',
+    linkedinTextarea: 'textarea:nth-child(4)',
+    positionInput: 'input[type=text]:nth-child(5)',
+    experienceTextarea: 'textarea:nth-child(6)',
+    skillsTextarea: 'textarea:nth-child(7)',
+    educationTextarea: 'textarea:nth-child(8)',
+    certificationsTextarea: 'textarea:nth-child(9)',
+    languagesTextarea: 'textarea:nth-child(10)',
+    submitButton: 'button',
+  };
+
+  it('Deve preencher o formulário com dados aleatórios do Faker.js e gerar um currículo', () => {
+    // Gerando dados aleatórios com Faker.js
+    const randomFirstName = faker.person.firstName();
+    const randomLastName = faker.person.lastName();
+    const randomName = `${randomFirstName} ${randomLastName}`; // Nome completo
+    const randomEmail = faker.internet.email({
+      firstName: randomFirstName,
+      lastName: randomLastName
+    }); 
+    const randomPhone = faker.phone.number('839########'); // Exemplo de formato de celular (ex: 83912345678)
+
+    cy.visit(APP_URL);
+
+    cy.get('h1').should('contain', 'Currículo Inteligente');
+
+    // Get the form element once to chain commands
+    const form = cy.get('#root > div > form');
+
+    // Fill in personal information with random data from Faker.js
+    form.find(selectors.nameInput)
+      .should('exist')
+      .type(randomName);
+
+    form.find(selectors.phoneInput)
+      .should('exist')
+      .type(randomPhone);
+
+    form.find(selectors.emailInput)
+      .should('exist')
+      .type(randomEmail);
+
+    form.find(selectors.linkedinTextarea)
+      .should('exist')
+      .type('https://www.youtube.com/watch?v=hQ01th1G1AE'); // Using the provided URL as is
+
+    // Fill in professional information
+    form.find(selectors.positionInput)
+      .should('exist')
+      .type(faker.person.jobTitle()); // Exemplo: Título de cargo aleatório
+
+    form.find(selectors.experienceTextarea)
+      .should('exist')
+      .type(faker.lorem.paragraphs(2)); // Exemplo: 2 parágrafos de texto aleatório para experiência
+
+    form.find(selectors.skillsTextarea)
+      .should('exist')
+      .type('Cypress, React, Node.js, Java, Python, SQL, MongoDB, Docker, Kubernetes, Selenium, Git, Agile, Scrum, Kanban, Test Automation, Continuous Integration, Continuous Deployment, DevOps, Cloud Computing, Microservices, RESTful APIs, GraphQL, Web Development, Mobile Development, UI/UX Design, Performance Optimization, Security Best Practices');
+
+    // Fill in academic and additional information
+    form.find(selectors.educationTextarea)
+      .should('exist')
+      .type('Graduação completa em Ciência da Computação pela Universidade Federal de Campina Grande, MBA em Engenharia de Software pela PUC-Rio');
+
+    form.find(selectors.certificationsTextarea)
+      .should('exist')
+      .type('CTFL, AWS Practitioner, Scrum Master, Docker Certified Associate');
+
+    form.find(selectors.languagesTextarea)
+      .should('exist')
+      .type('Ingles B2, Portugues Nativo, Espanhol A2');
+
+    // Submit the form
+    form.find(selectors.submitButton)
+      .should('exist')
+      .click();
+
+    // Add assertions for the next page/state if applicable (e.g., verify success message,
+    // or that the generated curriculum is displayed).
+  });
+});
